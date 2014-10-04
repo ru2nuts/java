86b74d4d8eb7915c841e05acdcc1a537c284a4a3
Alex A <alex@ru2nuts.com>
2014-10-04 19:40:41 -0400

    addign repeating elements tests

--- skyhook_code_review/src/test/java/example/QuickSortTest.java
+++ skyhook_code_review/src/test/java/example/QuickSortTest.java
@@ -13,6 +13,7 @@ public class QuickSortTest {
     Assert.assertEquals(QuickSort.sort(new int[]{3, 2, 1}), new int[]{1, 2, 3});
     Assert.assertEquals(QuickSort.sort(new int[]{1, 5, 3, 4, 8, 6}), new int[]{1, 3, 4, 5, 6, 8});
     Assert.assertEquals(QuickSort.sort(new int[]{1, 5, 3, 4, 8, 6, 0}), new int[]{0, 1, 3, 4, 5, 6, 8});
+    Assert.assertEquals(MergeSort.sort(new int[]{1, 5, 3, 5, 4, 8, 6, 5, 0}), new int[]{0, 1, 3, 4, 5, 5, 5, 6, 8});
   }
 
   @Test(expectedExceptions = AssertionError.class)
